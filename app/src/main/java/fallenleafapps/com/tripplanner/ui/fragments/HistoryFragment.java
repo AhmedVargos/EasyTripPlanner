819592786d61bcbcf346171d7faffb3f3479528c
ahmed ehab <ahmed_ehab707@yahoo.com>
2018-03-23 21:13:35 +0200

    Adding the map service and view to the history fragment

--- app/src/main/java/fallenleafapps/com/tripplanner/ui/fragments/HistoryFragment.java
+++ app/src/main/java/fallenleafapps/com/tripplanner/ui/fragments/HistoryFragment.java
@@ -1,17 +1,32 @@
 package fallenleafapps.com.tripplanner.ui.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import fallenleafapps.com.tripplanner.R;
 
-public class HistoryFragment extends Fragment {
+public class HistoryFragment extends Fragment implements OnMapReadyCallback {
 
+
+    @BindView(R.id.fragment_history_map)
+    MapView fragmentHistoryMap;
+    Unbinder unbinder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +37,39 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        //TODO query the DB for all the past trips and make lines and markers of them
+        fragmentHistoryMap.onCreate(savedInstanceState);
+        fragmentHistoryMap.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(34.088808,-118.40612), new LatLng(40.7, -74.0))
+                .width(5)
+                .color(Color.RED));
+
+        googleMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(25.767368,-80.18930), new LatLng(40.727093,-73.978639833333))
+                .width(5)
+                .color(Color.RED));
+
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.7, -74.0)).title("Source"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.088808,-118.40612)).title("Destination"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.727093,-73.978639833333), 11));
+        fragmentHistoryMap.onResume();
     }
 
     /**
