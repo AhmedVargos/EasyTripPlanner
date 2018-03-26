081eb0415ac5a8eff33734a13051cdfc43c2dfb2
yomnaghussein <yomna.g.hussein@gmail.com>
2018-03-26 08:39:16 +0200

    Merge branch 'master' of https://github.com/AhmedVargos/EasyTripPlanner
    # Conflicts:
    #	.idea/modules.xml

---
+++ app/src/main/java/fallenleafapps/com/tripplanner/ui/activities/AddTripActivity.java
@@ -0,0 +1,15 @@
+package fallenleafapps.com.tripplanner.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fallenleafapps.com.tripplanner.R;
+
+public class AddTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+}
